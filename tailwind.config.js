59d3fdfd085808c41fa72d38c9e2092b7bcf8c7b
Tortole <xorid@mail.ru>
2024-04-10 20:49:39 +0800

    feat: add base html

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                "red example": "#ff0000",
+                "green-leaf": "#39B54A",
+                "green-n": "#2BCF3C",
+                "green-l": "#74FF82",
+                "green-l-hover": "#1BAB43",
+                "green-l-onclick": "#0D9232",
+                "green-ll": "#EBFFEE",
+                "green-ll-hover": "#C6FFCE",
+                "green-ll-onclick": "#6CB877",
+                "red-l": "#FF7474",
+            },
+            fontFamily: {
+                play: ["Play"],
+                jockeyone: ["'Jockey One'"],
             },
         },
     },
