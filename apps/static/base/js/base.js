692d5222ea0c322ab97a9585e1871ed211c69560
Tortole <xorid@mail.ru>
2024-04-11 20:05:14 +0800

    feat add notification and account submenus

--- apps/static/base/js/base.js
+++ apps/static/base/js/base.js
@@ -1,8 +1,15 @@
-function toggleSidebar() {
-    let sidebar = $("#baseSidebar");
+hideElement("baseNotifications");
+hideElement("baseAccountSubmenu");
+
+function toggleElement(elementId) {
+    let sidebar = $(`#${elementId}`);
     if (sidebar.css("display") != "none") {
         sidebar.css("display", "none");
     } else {
         sidebar.css("display", "");
     }
+}
+
+function hideElement(elementId) {
+    $(`#${elementId}`).css("display", "none");
 }
