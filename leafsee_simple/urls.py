5bcbfaa66e48f38cac318f187efb9db25758b35e
Tortole <xorid@mail.ru>
2024-06-10 11:20:17 +0800

    feat: add base page

--- leafsee_simple/urls.py
+++ leafsee_simple/urls.py
@@ -18,6 +18,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+# !!!
+from django.views.generic import TemplateView
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    # !!!
+    path("temp/", TemplateView.as_view(template_name="base/base.html")),
 ]
