f441b99999d8b96b9d99cac7fada17e6d723e701
Tortole <xorid@mail.ru>
2024-08-28 20:34:59 +0800

    feat: LSS-13: add progress bar

---
+++ apps/videos/static/videos/js/watch.js
@@ -0,0 +1,48 @@
+const videoPlayer = $("#video-player");
+const video = videoPlayer.get(0);
+
+// >>>> Play button >>>>
+const playButton = $("#control-play-button");
+
+function togglePlayButton() {
+    playButton.children().each(function () {
+        $(this).toggleClass("hidden");
+    });
+}
+
+function togglePlay() {
+    if (video.paused || video.ended) {
+        video.play();
+    } else {
+        video.pause();
+    }
+}
+
+playButton.on("click", togglePlay);
+videoPlayer.on("click", togglePlay);
+videoPlayer.on("play", togglePlayButton);
+videoPlayer.on("pause", togglePlayButton);
+// <<<< <<<<
+
+// >>>> Progress Bar >>>>
+const progressBar = $("#control-progress-bar");
+const progressBarLoaded = $("#control-progress-bar-loaded");
+const progressBarWatched = $("#control-progress-bar-watched");
+
+function handleProgressBar() {
+    var progressPercentage = (video.currentTime / video.duration) * 100;
+    progressBarWatched.css("width", `${progressPercentage}%`);
+}
+
+function scrub(event) {
+    const scrubTime = (event.offsetX / progressBar.width()) * video.duration;
+    video.currentTime = scrubTime;
+}
+
+videoPlayer.on("timeupdate", handleProgressBar);
+progressBar.on("click", scrub);
+var mousedown = false;
+progressBar.on("mousedown", () => (mousedown = true));
+progressBar.on("mousemove", (event) => mousedown && scrub(event));
+progressBar.on("mouseup", () => (mousedown = false));
+// <<<< <<<<
