c8ff80a54858db1add77fd6e081b47d619c71150
Tortole <xorid@mail.ru>
2024-11-18 17:00:07 +0800

    dev: Add ReactJS and static serving with Nginx
    - moved Django `staticfiles` directory to `backend` for better backend-frontend split
    - Docker Compose
        - remove `version` attribute because it deprecated
        - changed Nginx Docker settings to compatibility with `nginx.conf` settings
        - changed PostgreSQL Docker container port to avoid conflicts on different machines
    - Nginx
        - added static and media files serving
        - added ReactJS serving
    - ReactJS
        - installed ReactJS project
        - integrate ReactJS frontend with Nginx
        - added ReactJS static files serving

--- backend/configs/settings.py
+++ backend/configs/settings.py
@@ -130,14 +130,14 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
-LOCALE_PATHS = [BASE_DIR.parent / "locale"]
+LOCALE_PATHS = [BASE_DIR / "locale"]
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / "apps/staticfiles"
-STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = "staticback/"
 STATICFILES_DIRS = [
     BASE_DIR / "apps/static",
 ]
